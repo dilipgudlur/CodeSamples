99706811adb1517c1bb3db9938f9604c62cd4a10
Dilip Gudlur <gdilip@gmail.com>
2018-01-12 14:17:21 -0800

    1.3
    Change-Id: Ie4a0c54165b6e6d4c92d2a931eb934777526bbf4

--- CCI/cci_1.2.cpp
+++ CCI/cci_1.2.cpp
@@ -5,7 +5,7 @@
 #include <algorithm>    // std::sort
 
 using namespace std;
-
+//soluion 1
 bool isAnagram(string& s1,string& s2) {
  
     size_t n = s1.size();
@@ -21,6 +21,35 @@ bool isAnagram(string& s1,string& s2) {
             return false;
     }
     
+    return true;
+}
+
+//solution 2
+bool isAnagram(string s1,string s2) {
+ 
+    size_t n = s1.size();
+    
+    if(n != s2.size())
+        return false;
+    
+    int charCount[128] = {0}; //remember to initialize otherwise undefined behavior
+    
+    //populate an array with the number of occurrences of chars in s1
+    for(int i = 0;i < (int)n;i++) {
+        int index = s1[i];
+        charCount[index]++;
+    }
+    
+    /*reduce the char count for each char of s2,
+    if char count for a char becomes -ve, means
+    char is present in s2 and not in s1, return false*/
+    for(int i = 0;i < (int)n;i++) {
+        int index = s2[i];
+        charCount[index]--;
+        if(charCount[index] < 0) {
+            return false;
+        }
+    }
     return true;
 }
 
---
+++ CCI/cci_1.3.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*1.3 URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient 
+space at the end to hold the additional characters, and that you are given the "true" length of the string.
+EXAMPLE Input: "Mr John Smith     ", 13 
+        Output: "Mr%20John%20Smith" pg90 SOLUTION 
+*/
+void URLify(string& s, int len) {
+    
+    size_t n = s.size();
+    int currentIndex = 0;
+    
+    //starting from the end move the chars back, find first non-space char
+    for(int i = n-1;i >= 0;--i) {
+        if(s[i] != ' ') {
+            currentIndex = i;
+            break;
+        }
+    }
+    
+    for(int i = n-1;i >= 0;) {
+        if(s[currentIndex] != ' ') {
+            s[i--] = s[currentIndex--];
+        }
+        else {//currentIndex is a space
+            s[i--] = '0'; 
+            s[i--] = '2';
+            s[i--] = '%';
+            currentIndex--;
+        }
+    }   
+}
+
+int main()
+{
+    string s = "Mr John Smith    ";
+    
+    URLify(s,13);
+    
+    cout << "URLified string is " << s << endl;
+        
+    return 0;
+}
+
