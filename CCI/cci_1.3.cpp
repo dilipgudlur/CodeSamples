981f001c39691cbf6a9068e9a3abb7b8255a815e
Dilip Gudlur <gdilip@gmail.com>
2018-01-12 14:19:46 -0800

    1.3
    Change-Id: I30e637e5d6d0045cd846fa245a01dd333e0f493c

--- CCI/cci_1.3.cpp
+++ CCI/cci_1.3.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 /*1.3 URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient 
 space at the end to hold the additional characters, and that you are given the "true" length of the string.
-EXAMPLE Input: "Mr John Smith     ", 13 
-        Output: "Mr%20John%20Smith" pg90 SOLUTION 
+EXAMPLE Input: "Mr John Smith     ", 
+        Output: "Mr%20John%20Smith" 
 */
 void URLify(string& s, int len) {
     
