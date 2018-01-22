b4c41fc415a9d0be181aff2c48d963f7e9422e79
Dilip Gudlur <gdilip@gmail.com>
2018-01-21 21:30:48 -0800

    2.5
    Change-Id: Iabf393b8db3c6a6558296a4999c18c419410aa84

---
+++ CCI/LinkedList/cci_2.5.cpp
@@ -0,0 +1,157 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+/*2.5 Sum Lists: You have two numbers represented by a linked list, where each node contains a single digit. 
+The digits are stored in reverse order, such that the 1 's digit is at the head of the list. Write a function 
+that adds the two numbers and returns the sum as a linked list. 
+EXAMPLE Input: (7-> 1 -> 6) + (5 -> 9 -> 2).Thatis,617 + 295. 
+Output: 2 -> 1 -> 9. That is, 912. 
+FOLLOW UP Suppose the digits are stored in forward order. Repeat the above problem. 
+Input: (6 -> 1 -> 7) + (2 -> 9 -> 5).Thatis,617 + 295. 
+Output: 9 -> 1 -> 2. That is, 912
+*/
+
+typedef struct node {
+    struct node* next;
+    int val;
+}NODE;
+
+NODE* headA;
+NODE* headB;
+NODE* headC;
+
+void addToList(NODE*& head, int val) {
+    if(!head) { //first element being added
+        head = new NODE;
+        if(head) {
+            head->next = NULL;
+            head->val = val;
+            //cout << "Adding " << head->val << endl;
+        }
+    }
+    else { //already elements are present
+        NODE* tmp = head;
+        while(tmp->next)
+            tmp = tmp->next;
+        NODE* node = new NODE;
+        if(node) {
+            tmp->next = node;
+            node->next = NULL;
+            node->val = val;
+            //cout << "Adding " << node->val << endl;
+        }
+    }
+}
+
+//Recursive solution
+void sumDigitsInReverse(NODE* headA, NODE* headB,int carry) {
+    int tmp = 0;
+    int sum = 0;
+    bool AisNULL = true;
+    bool BisNULL = true;
+    
+    if(!headA && !headB) {
+        if(carry)
+            addToList(headC,carry);
+        return;
+    }
+    
+    if(headA) {
+        tmp += headA->val;
+        AisNULL = false;
+    }
+    
+    if(headB) {
+        tmp += headB->val;
+        BisNULL = false;
+    }
+    
+    tmp += carry;
+    sum = tmp%10;
+    carry = tmp/10;
+    
+    addToList(headC,sum);
+    
+    if(AisNULL)
+        sumDigitsInReverse(NULL,headB->next, carry);
+    else if(BisNULL)
+        sumDigitsInReverse(headA->next,NULL, carry);
+    else
+        sumDigitsInReverse(headA->next,headB->next, carry);
+}
+
+//Iterative solution
+void sumDigitsInReverse(NODE* headA, NODE* headB) {
+    
+    NODE* l1 = headA;
+    NODE* l2 = headB;
+    
+    if(!l1 && !l2)
+        return;
+    
+    int sum = 0;
+    int carry = 0;
+    
+    while(l1 && l2) {
+        sum = (l1->val + l2->val + carry)%10;      
+        addToList(headC,sum);
+        carry = (l1->val + l2->val + carry)/10;
+        l1 = l1->next;
+        l2 = l2->next;
+    }
+    
+    while(l1) {
+        sum = (l1->val + carry)%10;
+        addToList(headC,sum);
+        carry = (l1->val + carry)/10;
+        l1 = l1->next;
+    }
+    
+    while(l2) {
+        sum = (l2->val + carry)%10;
+        addToList(headC,sum);
+        carry = (l2->val + carry)/10;
+        l2 = l2->next;
+    }
+    
+    if(carry) { //carry can be zero
+        addToList(headC,carry);
+    }
+}
+
+void showList(NODE* head) {
+    if(head) {
+        NODE* tmp = head;
+        while(tmp != NULL) {
+            cout << tmp->val << "->";
+            tmp = tmp->next;
+        }
+        cout << endl;
+    }
+    else
+        cout << "No List to display" << endl;
+}
+
+int main()
+{
+    vector<int> a = {7,1,6,9}; //input for first list
+    vector<int> b = {5,9,4}; //input for second list
+    
+    for(int i = 0;i < (int)a.size();i++)
+        addToList(headA, a[i]);
+    for(int i = 0;i < (int)b.size();i++)
+        addToList(headB, b[i]);
+    cout << "Original List is " << endl;
+    showList(headA);
+    showList(headB);
+    
+    sumDigitsInReverse(headA,headB,0);
+    //sumDigitsInReverse(headA,headB);
+    cout << "Sum is " << endl;
+    showList(headC);
+    return 0;
+}
