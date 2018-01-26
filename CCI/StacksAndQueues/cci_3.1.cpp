646c4ed05858c691e1530bbf4e9d3265651e11f4
Dilip Gudlur <gdilip@gmail.com>
2018-01-26 15:57:19 -0800

    3.1
    Change-Id: I48bb41ee8179f8f935e1cb8dc54c0abcac9652b0

--- CCI/StacksAndQueues/cci_3.1.cpp
+++ CCI/StacksAndQueues/cci_3.1.cpp
@@ -1,9 +1,9 @@
-
 #include <iostream>
 #include <string>
 #include <unordered_map>
 #include <vector>
 #include <stack>
+#include <limits.h>
 
 using namespace std;
 
@@ -21,40 +21,78 @@ class FixedStack {
 private:
     int numStacks;
     vector<int> stackBuffer;
+    vector<int> topIndex;
+    int stackCapacity;
 
 public:
-    FixedStack(int x, int y): stackBuffer(x,0) { //initializes stack buffer to size x
-        numStacks = y;
-        for(int i = 0;i < x;i++) {
-            cout << stackBuffer[i] <<endl;
+    FixedStack(int x, int y): stackBuffer(x,INT_MIN)  { //initializes stack buffer to size x
+        if(x < 1 || y < 1) {
+            cout << "Improper Stack Sizes" << endl;
+            return;
         }
+        numStacks = y;
+        /*for(int i = 0;i < x;i++) {
+            cout << stackBuffer[i] <<endl;
+        }*/
+        vector<int> tmp(y,0); //initialize topIndex of each stack with 0
+        topIndex = tmp;
+        /*for(int i = 0;i < y;i++) {
+            cout << topIndex[i] <<endl;
+        }*/
+        stackCapacity = x/y;
     }
-    void pop();
+    void pop(int stackIndex);
     void push(int stackIndex, int val);
     bool isEmpty(int stackIndex);
+    bool isFull(int stackIndex);
     int top(int stackIndex);
 };
 
 void FixedStack::push(int stackIndex, int val) {
+    if(!isFull(stackIndex)) {
+        int index = (stackCapacity * stackIndex) + topIndex[stackIndex];
+        stackBuffer[index] = val;
+        topIndex[stackIndex]++;
+    }
+    
 }
 
-void FixedStack::pop() {
+void FixedStack::pop(int stackIndex) {
+    if(!isEmpty(stackIndex)) {
+        int index = (stackCapacity * stackIndex) + topIndex[stackIndex]-1;
+        stackBuffer[index] = INT_MIN;
+        topIndex[stackIndex]--;
+    }
 }
 
 int FixedStack::top(int stackIndex) {
-    return 0;
+    int index = (stackCapacity * stackIndex) + topIndex[stackIndex]-1;
+    int result = stackBuffer[index];
+    cout << "Top of stack " << stackIndex+1 << " is " << result << endl;
+    return stackBuffer[index];
 }
 
 bool FixedStack::isEmpty(int stackIndex) {
+    if(!topIndex[stackIndex])
+        return true;
+    return false;
+}
+
+bool FixedStack::isFull(int stackIndex) {
+    if(topIndex[stackIndex] == stackCapacity)
+        return true;
     return false;
 }
 
 int main() {
     
-    FixedStack fs(12,3);
+    int bufferSize = 12;
+    int numStacks = 3;
+    FixedStack fs(bufferSize,numStacks);
     fs.push(0,1);
     fs.push(1,5);
     fs.push(2,3);
+    fs.pop(1);
     fs.top(0);
     fs.top(1);
     fs.top(2);
