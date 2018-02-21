b9bc1d7ddd4eb767c11ffeea1ae9d435ba70d5e5
Dilip Gudlur <gdilip@gmail.com>
2018-02-21 01:26:51 -0800

    4.4
    Change-Id: If005eb3a4195385d9147e6517e6b711b7acce982

--- CCI/TreesAndGraphs/cci_4.4.cpp
+++ CCI/TreesAndGraphs/cci_4.4.cpp
@@ -2,7 +2,12 @@
 */
 
 //Balanced: at any node, difference in height of left subtree and right subtree should not be more than 1
-int height(node* root) { //calculates height of a tree
+
+/*Approach1: this is simpler, but makes lotof unncessary calls to nodes whose height has been already calculated
+Each subtree takes lgn time, but since this executes for all n nodes, it takes O(nlgn) time
+*/
+
+/*int height(node* root) { //calculates height of a tree
 
     if(!root)
         return -1;
@@ -20,6 +25,41 @@ bool isBalanced(node* root) {
         return false;
     else
         return true;
+}*/
+
+
+//Approach2: Here we try to prevent making unnecessary calls if we already know if a subtree is unbalanced
+//Runs in O(n) time and O(H) space where H is the height of the tree
+ 
+int height(node* root) { //calculates height of a tree
+
+    if(!root)
+        return -1;
+    
+    if(!root->left && !root->right) //cuts down two calls if node is leaf
+        return 0;
+    
+    int leftHeight = height(root->left);
+    if(leftHeight == INT_MIN) //we already know left subtree is not balanced, so propagate that without checking for right
+        return INT_MIN;
+    
+    int rightHeight = height(root->right);
+    if(rightHeight == INT_MIN)
+        return INT_MIN;//we already know left subtree is not balanced, so propagate that without checking for right
+    
+    if(abs(leftHeight - rightHeight) > 1)
+        return INT_MIN; //use this as an error code to propagate upwards without going ahead to make further recursive calls
+    else
+        return max(leftHeight,rightHeight) + 1; 
+}
+    
+bool isBalanced(node* root) {
+    if(!root)
+        return false;
+    
+    int h = height(root);
+    cout << "height is " << h << endl;
+    return h == INT_MIN ? false: true;
 }
 
 int main()
